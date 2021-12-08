d928d8ba964c81967b961136a307fd9b4c6172f0
theratzoo <lderatzou@wpi.edu>
2021-12-08 09:47:22 -0500

    added some actual data and loaded it to catalog page

--- pages/catalog.js
+++ pages/catalog.js
@@ -1,15 +1,17 @@
 import MyNavbar from './navbar'
 import Head from 'next/head'
+import Script from 'next/script'
+import Link from 'next/link'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // TODO: add paddingRight to all th and td to make spacing look nice!
-// TODO: figure out how to programatically load in data from .csv/.json
-// file every time. this is a task i wanna figure out ASAP!
-
+// TODO: make a table that can associate deckbox.org card IDs to exact card printings.
+// this is so i can get the SPECIFIC version of cards when you hover over them. this is low prio
 
 
 export default function Catalog() {
-
+	const data = require('dsv-loader!./mtg_card_catalog.csv')
+	console.log(data)
 	return (
 		<div>
 			<Head>
@@ -20,8 +22,10 @@ export default function Catalog() {
 				<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
 				<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+				
       		</Head>
 			<main>
+				<Script src="https://deckbox.org/assets/external/tooltip.js"></Script>
 				<MyNavbar/>
 				<div className="container">
 					<div className="jumbotron">
@@ -31,22 +35,28 @@ export default function Catalog() {
 					<h2>Note: this is currently a big WIP, with features being developed as we speak!</h2>
 					<br/>
 					<table>
-						<tr>
-							<th>Card Name</th>&nbsp;&nbsp;&nbsp;&nbsp;
-							<th>Quantity</th>&nbsp;&nbsp;&nbsp;&nbsp;
-							<th>Set</th>&nbsp;&nbsp;&nbsp;&nbsp;
-							<th>Condition</th>&nbsp;&nbsp;&nbsp;&nbsp;
-							<th>Foil</th>&nbsp;&nbsp;&nbsp;&nbsp;
-							<th>Price</th>&nbsp;&nbsp;&nbsp;&nbsp;
-						</tr>
-						<tr>
-							<td>Angelic Chorus</td>&nbsp;&nbsp;&nbsp;&nbsp;
-							<td>1</td>&nbsp;&nbsp;&nbsp;&nbsp;
-							<td>USG</td>&nbsp;&nbsp;&nbsp;&nbsp;
-							<td>MP</td>&nbsp;&nbsp;&nbsp;&nbsp;
-							<td>No</td>&nbsp;&nbsp;&nbsp;&nbsp;
-							<td>$1.29</td>&nbsp;&nbsp;&nbsp;&nbsp;
-						</tr>
+						<thead>
+							<tr>
+								<th>Card Name</th>&nbsp;&nbsp;&nbsp;&nbsp;
+								<th>Quantity</th>&nbsp;&nbsp;&nbsp;&nbsp;
+								<th>Set</th>&nbsp;&nbsp;&nbsp;&nbsp;
+								<th>Condition</th>&nbsp;&nbsp;&nbsp;&nbsp;
+								<th>Foil</th>&nbsp;&nbsp;&nbsp;&nbsp;
+								<th>Price</th>&nbsp;&nbsp;&nbsp;&nbsp;
+							</tr>
+						</thead>
+						<tbody>
+							{data.map((card, i)=>
+							<tr key={i}>
+								<td><Link href={"https://deckbox.org/mtg/" + card["Card Name"]}>{card["Card Name"]}</Link></td>&nbsp;&nbsp;&nbsp;&nbsp;
+								<td>{card["Quantity"]}</td>&nbsp;&nbsp;&nbsp;&nbsp;
+								<td>{card["Set"]}</td>&nbsp;&nbsp;&nbsp;&nbsp;
+								<td>{card["Condition"]}</td>&nbsp;&nbsp;&nbsp;&nbsp;
+								<td>{card["Foil"]}</td>&nbsp;&nbsp;&nbsp;&nbsp;
+								<td>{"PLACEHOLDER PRICE"}</td>&nbsp;&nbsp;&nbsp;&nbsp;
+							</tr>
+							)}
+						</tbody>
 					</table>
 				</div>
 			</main>
